17b4cacdce046eb0ac670700095dbb11f7e20a4b
Pedro H R Campos <pedrohenriqueribei@gmail.com>
2023-06-25 17:08:16 -0300

    Implementando End-Point de salvar cozinha com retorno e código HTTP de resposta correto

--- src/main/java/com/algaworks/algafood/controller/CozinhaController.java
+++ src/main/java/com/algaworks/algafood/controller/CozinhaController.java
@@ -3,10 +3,13 @@ package com.algaworks.algafood.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -46,6 +49,15 @@ public class CozinhaController {
 			return ResponseEntity.ok(cozinha);
 		}
 		return ResponseEntity.notFound().build();
+	}
+	
+	@PostMapping
+	public ResponseEntity<Cozinha> salvar(@RequestBody Cozinha cozinha) {
+		System.out.println("Chegou no método salvar");
+		
+		Cozinha cozinhaSalvada = cozinhaRepository.salvar(cozinha);
+		
+		return ResponseEntity.status(HttpStatus.CREATED).body(cozinhaSalvada);
 	}
 
 }
