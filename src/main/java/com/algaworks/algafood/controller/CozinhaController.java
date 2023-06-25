e3333763734ddafec6e322ce7785745048c48c20
Pedro H R Campos <pedrohenriqueribei@gmail.com>
2023-06-25 12:55:52 -0300

    Manipulando o status HTTP da resposta

--- src/main/java/com/algaworks/algafood/controller/CozinhaController.java
+++ src/main/java/com/algaworks/algafood/controller/CozinhaController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -34,9 +35,14 @@ public class CozinhaController {
 		return new CozinhasXmlWrapper(cozinhaRepository.todas());
 	}
 	
+	//jeito 1 de retornar status especificado
+	//@ResponseStatus(value = HttpStatus.ACCEPTED)
 	@GetMapping( "{id}")
-	public Cozinha buscar(@PathVariable Long id) {
-		return cozinhaRepository.buscar(id);
+	public ResponseEntity<Cozinha> buscar(@PathVariable Long id) {
+		Cozinha cozinha = cozinhaRepository.buscar(id);
+		
+		//return ResponseEntity.status(203).body(cozinha);
+		return ResponseEntity.ok(cozinha);
 	}
 
 }
