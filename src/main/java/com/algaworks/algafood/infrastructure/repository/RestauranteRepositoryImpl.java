56bc9136cd845ffde3e0b8bca2f9dddaaf2200a4
Pedro H R Campos <pedrohenriqueribei@gmail.com>
2023-07-31 21:51:28 -0300

    5.12. Implementando uma consulta dinâmica com JPQL

--- src/main/java/com/algaworks/algafood/infrastructure/repository/RestauranteRepositoryImpl.java
+++ src/main/java/com/algaworks/algafood/infrastructure/repository/RestauranteRepositoryImpl.java
@@ -1,12 +1,15 @@
 package com.algaworks.algafood.infrastructure.repository;
 
 import java.math.BigDecimal;
+import java.util.HashMap;
 import java.util.List;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
 
 import org.springframework.stereotype.Repository;
+import org.springframework.util.StringUtils;
 
 import com.algaworks.algafood.domain.model.Restaurante;
 
@@ -16,15 +19,55 @@ public class RestauranteRepositoryImpl implements RestauranteRepositoryQueries {
 	@PersistenceContext
 	private EntityManager manager;
 	
-	@Override
+	
+	/**
+	 * método com parametros obrigatórios
+	 */
+//	@Override
+//	public List<Restaurante> find (String nome, BigDecimal taxaInicial, BigDecimal taxaFinal) {
+//		
+//		var jpql = "from Restaurante where nome like :nome and taxaFrete between :taxaInicial and :taxaFinal";
+//		
+//		return manager.createQuery(jpql, Restaurante.class)
+//				.setParameter("nome", "%"+nome+"%")
+//				.setParameter("taxaInicial", taxaInicial)
+//				.setParameter("taxaFinal", taxaFinal)
+//				.getResultList();
+//	}
+	
+	/**
+	 * método com parâmetros opcionais com JPQL
+	 */
 	public List<Restaurante> find (String nome, BigDecimal taxaInicial, BigDecimal taxaFinal) {
 		
-		var jpql = "from Restaurante where nome like :nome and taxaFrete between :taxaInicial and :taxaFinal";
+		var jpql = new StringBuilder();
+		jpql.append("from Restaurante where 0=0");
 		
-		return manager.createQuery(jpql, Restaurante.class)
-				.setParameter("nome", "%"+nome+"%")
-				.setParameter("taxaInicial", taxaInicial)
-				.setParameter("taxaFinal", taxaFinal)
-				.getResultList();
+		var parametros = new HashMap<String, Object>();
+		
+		//verificar se o nome é nulo ou vazio
+		if(StringUtils.hasLength(nome)) {
+			jpql.append("and nome like :nome ");
+			parametros.put("nome", "%" + nome + "%");
+		}
+		
+		//verificar se taxa é nulo
+		if(taxaInicial != null) {
+			jpql.append("and taxaFrete >= :taxaInicial ");
+			parametros.put("taxaInicial", taxaInicial);
+		}
+		
+		//verificar se taxa é nulo
+		if (taxaFinal != null) {
+			jpql.append("and taxaFrete <= :taxaFinal ");
+			parametros.put("taxaFinal", taxaFinal);
+		}
+		
+		TypedQuery<Restaurante> query = manager.createQuery(jpql.toString(), Restaurante.class);
+		
+		parametros.forEach((chave, valor) -> query.setParameter(chave, valor));
+		
+		return query.getResultList();
+				
 	}
 }
