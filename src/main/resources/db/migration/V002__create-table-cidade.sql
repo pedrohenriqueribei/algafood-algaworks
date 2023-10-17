6485784a4cbe4bd73d6f8494687f105a539cdedf
Pedro H R Campos <pedrohenriqueribei@gmail.com>
2023-10-16 21:48:53 -0300

    7.7. Evoluindo o banco de dados com novas migrações

--- src/main/resources/db/migration/V002__create-table-cidade.sql
+++ src/main/resources/db/migration/V002__create-table-cidade.sql
@@ -1,6 +1,14 @@
 CREATE TABLE tb_cidade(
 	id bigint not null AUTO_INCREMENT,
     nome VARCHAR(60) NOT NULL,
-    nome_estado VARCHAR(80),
+    estado_id bigint not null,
     PRIMARY KEY (id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE tb_estado (
+	id bigint not null AUTO_INCREMENT,
+	nome VARCHAR(60) NOT NULL,
+	PRIMARY KEY (id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+ALTER TABLE tb_cidade ADD CONSTRAINT fk_cidade_estado FOREIGN KEY (estado_id) REFERENCES tb_estado (id);
