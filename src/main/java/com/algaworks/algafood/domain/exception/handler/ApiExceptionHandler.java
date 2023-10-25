1536154eba57e52a4a065413851cb51d19add157
Pedro H R Campos <pedrohenriqueribei@gmail.com>
2023-10-24 22:02:28 -0300

    8.13. Tratando exceções globais com @ExceptionHandler e @ControllerAdvice

---
+++ src/main/java/com/algaworks/algafood/domain/exception/handler/ApiExceptionHandler.java
@@ -0,0 +1,40 @@
+package com.algaworks.algafood.domain.exception.handler;
+
+import java.time.LocalDateTime;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.algaworks.algafood.domain.exception.EntidadeNaoEncontradaException;
+import com.algaworks.algafood.domain.exception.NegocioException;
+
+/*
+ * 8.13. Tratando exceções globais com @ExceptionHandler e @ControllerAdvice
+ */
+@ControllerAdvice
+public class ApiExceptionHandler {
+
+	@ExceptionHandler(EntidadeNaoEncontradaException.class)
+	public ResponseEntity<?> tratarEntidadeNaoEncontrada (EntidadeNaoEncontradaException e) {
+		
+		Problema problema = Problema.builder()
+				.dataHora(LocalDateTime.now())
+				.mensagem(e.getMessage())
+				.build();
+		
+		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(problema);
+	}
+	
+	@ExceptionHandler(NegocioException.class)
+	public ResponseEntity<?> tratarNegocioException (NegocioException e) {
+		
+		Problema problema = Problema.builder()
+				.dataHora(LocalDateTime.now())
+				.mensagem(e.getMessage())
+				.build();
+		
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(problema);
+	}
+}
