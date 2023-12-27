48f46065fb7628a229d33cade8020036560c44c8
Pedro H R Campos <pedrohenriqueribei@gmail.com>
2023-12-27 19:35:16 -0300

    12.6. Adicionando endereço no modelo da representação do recurso de restaurante

---
+++ src/main/java/com/algaworks/algafood/api/model/DTO/output/CidadeResumoDTO.java
@@ -0,0 +1,14 @@
+package com.algaworks.algafood.api.model.DTO.output;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class CidadeResumoDTO {
+
+		private Long id;
+		private String nome;
+		private String nomeEstado;
+
+}
